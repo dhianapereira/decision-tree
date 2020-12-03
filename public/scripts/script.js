22ed9a4b1678c6693e06ce1e5a648c06ee736297
Dhiana <dhianaslv@outlook.com>
2020-12-03 18:35:00 -0300

    using json file

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,492 +1,99 @@
-var data = [
-  {
-    class: "no-recurrence-events",
-    age: 49,
+function loadData() {
+  var data = [];
+  
+  $.getJSON("../../src/data.json", function (json) {
+    data = json;
+    console.log(data);
+  });
+
+  drawingTheDecisionTree(data);
+}
+
+function drawingTheDecisionTree(data) {
+  var config = {
+    trainingSet: data,
+    categoryAttr: "class",
+  };
+
+  var decisionTree = new dt.DecisionTree(config);
+
+  var patient = {
+    age: [30, 31, 32, 33, 34, 35, 36, 37, 38, 39],
     menopause: "premeno",
-    tumorSize: 20,
-    invNodes: 2,
-    nodeCaps: "no",
-    degMalig: 2,
+    tumorSize: [30, 31, 32, 33, 34],
+    degMalig: 3,
     breast: "left",
     breastQuad: "left_low",
-    irradiat: "no",
-  },
-  {
-    class: "no-recurrence-events",
-    age: 40,
-    menopause: "premeno",
-    tumorSize: 24,
-    invNodes: 2,
-    nodeCaps: "no",
-    degMalig: 2,
-    breast: "right",
-    breastQuad: "right_up",
-    irradiat: "no",
-  },
-  {
-    class: "recurrence-events",
-    age: 50,
-    menopause: "premeno",
-    tumorSize: 19,
-    invNodes: 0,
-    nodeCaps: "no",
-    degMalig: 2,
-    breast: "left",
-    breastQuad: "left_low",
-    irradiat: "no",
-  },
-  {
-    class: "no-recurrence-events",
-    age: 40,
-    menopause: "premeno",
-    tumorSize: 24,
-    invNodes: 2,
-    nodeCaps: "no",
-    degMalig: 2,
-    breast: "right",
-    breastQuad: "right_up",
-    irradiat: "no",
-  },
-  {
-    class: "no-recurrence-events",
-    age: 40,
-    menopause: "premeno",
-    tumorSize: 24,
-    invNodes: 2,
-    nodeCaps: "no",
-    degMalig: 2,
-    breast: "right",
-    breastQuad: "right_up",
-    irradiat: "no",
-  },
-  {
-    class: "no-recurrence-events",
-    age: 40,
-    menopause: "premeno",
-    tumorSize: 24,
-    invNodes: 2,
-    nodeCaps: "no",
-    degMalig: 2,
-    breast: "right",
-    breastQuad: "right_up",
-    irradiat: "no",
-  },
-  {
-    class: "no-recurrence-events",
-    age: 40,
-    menopause: "premeno",
-    tumorSize: 24,
-    invNodes: 2,
-    nodeCaps: "no",
-    degMalig: 2,
-    breast: "right",
-    breastQuad: "right_up",
-    irradiat: "no",
-  },
-  {
-    class: "no-recurrence-events",
-    age: 40,
-    menopause: "premeno",
-    tumorSize: 24,
-    invNodes: 2,
-    nodeCaps: "no",
-    degMalig: 2,
-    breast: "right",
-    breastQuad: "right_up",
-    irradiat: "no",
-  },
-  {
-    class: "no-recurrence-events",
-    age: 40,
-    menopause: "premeno",
-    tumorSize: 24,
-    invNodes: 2,
-    nodeCaps: "no",
-    degMalig: 2,
-    breast: "right",
-    breastQuad: "right_up",
-    irradiat: "no",
-  },
-  {
-    class: "no-recurrence-events",
-    age: 40,
-    menopause: "premeno",
-    tumorSize: 24,
-    invNodes: 2,
-    nodeCaps: "no",
-    degMalig: 2,
-    breast: "right",
-    breastQuad: "right_up",
-    irradiat: "no",
-  },
-  {
-    class: "no-recurrence-events",
-    age: 40,
-    menopause: "premeno",
-    tumorSize: 24,
-    invNodes: 2,
-    nodeCaps: "no",
-    degMalig: 2,
-    breast: "right",
-    breastQuad: "right_up",
-    irradiat: "no",
-  },
-  {
-    class: "no-recurrence-events",
-    age: 40,
-    menopause: "premeno",
-    tumorSize: 24,
-    invNodes: 2,
-    nodeCaps: "no",
-    degMalig: 2,
-    breast: "right",
-    breastQuad: "right_up",
-    irradiat: "no",
-  },
-  {
-    class: "no-recurrence-events",
-    age: 40,
-    menopause: "premeno",
-    tumorSize: 24,
-    invNodes: 2,
-    nodeCaps: "no",
-    degMalig: 2,
-    breast: "right",
-    breastQuad: "right_up",
-    irradiat: "no",
-  },
-  {
-    class: "no-recurrence-events",
-    age: 40,
-    menopause: "premeno",
-    tumorSize: 24,
-    invNodes: 2,
-    nodeCaps: "no",
-    degMalig: 2,
-    breast: "right",
-    breastQuad: "right_up",
-    irradiat: "no",
-  },
-  {
-    class: "no-recurrence-events",
-    age: 40,
-    menopause: "premeno",
-    tumorSize: 24,
-    invNodes: 2,
-    nodeCaps: "no",
-    degMalig: 2,
-    breast: "right",
-    breastQuad: "right_up",
-    irradiat: "no",
-  },
-  {
-    class: "no-recurrence-events",
-    age: 40,
-    menopause: "premeno",
-    tumorSize: 24,
-    invNodes: 2,
-    nodeCaps: "no",
-    degMalig: 2,
-    breast: "right",
-    breastQuad: "right_up",
-    irradiat: "no",
-  },
-  {
-    class: "no-recurrence-events",
-    age: 40,
-    menopause: "premeno",
-    tumorSize: 24,
-    invNodes: 2,
-    nodeCaps: "no",
-    degMalig: 2,
-    breast: "right",
-    breastQuad: "right_up",
-    irradiat: "no",
-  },
-  {
-    class: "no-recurrence-events",
-    age: 40,
-    menopause: "premeno",
-    tumorSize: 24,
-    invNodes: 2,
-    nodeCaps: "no",
-    degMalig: 2,
-    breast: "right",
-    breastQuad: "right_up",
-    irradiat: "no",
-  },
-  {
-    class: "no-recurrence-events",
-    age: 40,
-    menopause: "premeno",
-    tumorSize: 24,
-    invNodes: 2,
-    nodeCaps: "no",
-    degMalig: 2,
-    breast: "right",
-    breastQuad: "right_up",
-    irradiat: "no",
-  },
-  {
-    class: "no-recurrence-events",
-    age: 40,
-    menopause: "premeno",
-    tumorSize: 24,
-    invNodes: 2,
-    nodeCaps: "no",
-    degMalig: 2,
-    breast: "right",
-    breastQuad: "right_up",
-    irradiat: "no",
-  },
-  {
-    class: "no-recurrence-events",
-    age: 40,
-    menopause: "premeno",
-    tumorSize: 24,
-    invNodes: 2,
-    nodeCaps: "no",
-    degMalig: 2,
-    breast: "right",
-    breastQuad: "right_up",
-    irradiat: "no",
-  },
-  {
-    class: "no-recurrence-events",
-    age: 40,
-    menopause: "premeno",
-    tumorSize: 24,
-    invNodes: 2,
-    nodeCaps: "no",
-    degMalig: 2,
-    breast: "right",
-    breastQuad: "right_up",
-    irradiat: "no",
-  },
-  {
-    class: "no-recurrence-events",
-    age: 40,
-    menopause: "premeno",
-    tumorSize: 24,
-    invNodes: 2,
-    nodeCaps: "no",
-    degMalig: 2,
-    breast: "right",
-    breastQuad: "right_up",
-    irradiat: "no",
-  },
-  {
-    class: "no-recurrence-events",
-    age: 40,
-    menopause: "premeno",
-    tumorSize: 24,
-    invNodes: 2,
-    nodeCaps: "no",
-    degMalig: 2,
-    breast: "right",
-    breastQuad: "right_up",
-    irradiat: "no",
-  },
-  {
-    class: "no-recurrence-events",
-    age: 40,
-    menopause: "premeno",
-    tumorSize: 24,
-    invNodes: 2,
-    nodeCaps: "no",
-    degMalig: 2,
-    breast: "right",
-    breastQuad: "right_up",
-    irradiat: "no",
-  },
-  {
-    class: "no-recurrence-events",
-    age: 40,
-    menopause: "premeno",
-    tumorSize: 24,
-    invNodes: 2,
-    nodeCaps: "no",
-    degMalig: 2,
-    breast: "right",
-    breastQuad: "right_up",
-    irradiat: "no",
-  },
-  {
-    class: "no-recurrence-events",
-    age: 40,
-    menopause: "premeno",
-    tumorSize: 24,
-    invNodes: 2,
-    nodeCaps: "no",
-    degMalig: 2,
-    breast: "right",
-    breastQuad: "right_up",
-    irradiat: "no",
-  },
-  {
-    class: "no-recurrence-events",
-    age: 40,
-    menopause: "premeno",
-    tumorSize: 24,
-    invNodes: 2,
-    nodeCaps: "no",
-    degMalig: 2,
-    breast: "right",
-    breastQuad: "right_up",
-    irradiat: "no",
-  },
-  {
-    class: "no-recurrence-events",
-    age: 40,
-    menopause: "premeno",
-    tumorSize: 24,
-    invNodes: 2,
-    nodeCaps: "no",
-    degMalig: 2,
-    breast: "right",
-    breastQuad: "right_up",
-    irradiat: "no",
-  },
-  {
-    class: "no-recurrence-events",
-    age: 40,
-    menopause: "premeno",
-    tumorSize: 24,
-    invNodes: 2,
-    nodeCaps: "no",
-    degMalig: 2,
-    breast: "right",
-    breastQuad: "right_up",
-    irradiat: "no",
-  },
-  {
-    class: "no-recurrence-events",
-    age: 40,
-    menopause: "premeno",
-    tumorSize: 24,
-    invNodes: 2,
-    nodeCaps: "no",
-    degMalig: 2,
-    breast: "right",
-    breastQuad: "right_up",
-    irradiat: "no",
-  },
-  {
-    class: "no-recurrence-events",
-    age: 40,
-    menopause: "premeno",
-    tumorSize: 24,
-    invNodes: 2,
-    nodeCaps: "no",
-    degMalig: 2,
-    breast: "right",
-    breastQuad: "right_up",
-    irradiat: "no",
-  },
-];
+  };
 
-var config = {
-  trainingSet: data,
-  categoryAttr: "class",
-  ignoredAttributes: ['irradiat'],
-};
+  document.getElementById("testingItem").innerHTML =
+    "<td>" +
+    patient.age +
+    "</td>" +
+    "<td>" +
+    patient.menopause +
+    "</td>" +
+    "<td>" +
+    patient.tumorSize +
+    "</td>" +
+    "<td>" +
+    patient.degMalig +
+    "</td>" +
+    "<td>" +
+    patient.breast +
+    "</td>" +
+    "<td>" +
+    patient.breastQuad +
+    "</td>";
 
-var decisionTree = new dt.DecisionTree(config);
+  var decisionTreePrediction = decisionTree.predict(patient);
 
-var numberOfTrees = 3;
-var randomForest = new dt.RandomForest(config, numberOfTrees);
+  document.getElementById("decisionTreePrediction").innerHTML = decisionTreePrediction;
 
-var patient = {
-  age: 30,
-  menopause: "premeno",
-  tumorSize: 30,
-  invNodes: 0,
-  nodeCaps: "no",
-  degMalig: 3,
-  breast: "left",
-  breastQuad: "left_low",
-  irradiat: "no",
-};
 
-var decisionTreePrediction = decisionTree.predict(patient);
+  document.getElementById("displayTree").innerHTML = treeToHtml(
+    decisionTree.root
+  );
 
-document.getElementById("testingItem").innerHTML =
-  "Idade: " +
-  patient.age +
-  "<br>" +
-  "Menopausa: " +
-  patient.menopause +
-  "<br>" +
-  "Tamanho do tumor: " +
-  patient.tumorSize +
-  "<br>" +
-  "Inv. Nó: " +
-  patient.invNodes +
-  "<br>" +
-  "Nó: " +
-  patient.degMalig +
-  "<br>" +
-  "Grau: " +
-  patient.degMalig +
-  "<br>" +
-  "Mama: " +
-  patient.breast +
-  "<br>" +
-  "Peito-quadril: " +
-  patient.breastQuad +
-  "<br>" +
-  "Irradiar: " +
-  patient.irradiat +
-  "<br>";
+  function treeToHtml(tree) {
+    if (tree.category) {
+      return [
+        "<ul>",
+        "<li>",
+        '<a href="#">',
+        "<b>",
+        tree.category,
+        "</b>",
+        "</a>",
+        "</li>",
+        "</ul>",
+      ].join("");
+    }
 
-document.getElementById("decisionTreePrediction").innerHTML = JSON.stringify(
-  decisionTreePrediction,
-  null,
-  0
-);
-
-document.getElementById("displayTree").innerHTML = treeToHtml(
-  decisionTree.root
-);
-
-function treeToHtml(tree) {
-  if (tree.category) {
     return [
       "<ul>",
       "<li>",
       '<a href="#">',
       "<b>",
-      tree.category,
-      "</b>",
+      tree.attribute,
+      " ",
+      tree.predicateName,
+      " ",
+      tree.pivot,
+      " ?</b>",
       "</a>",
+      "<ul>",
+      "<li>",
+      '<a href="#">yes</a>',
+      treeToHtml(tree.match),
+      "</li>",
+      "<li>",
+      '<a href="#">no</a>',
+      treeToHtml(tree.notMatch),
+      "</li>",
+      "</ul>",
       "</li>",
       "</ul>",
     ].join("");
   }
-
-  return [
-    "<ul>",
-    "<li>",
-    '<a href="#">',
-    "<b>",
-    tree.attribute,
-    " ",
-    tree.predicateName,
-    " ",
-    tree.pivot,
-    " ?</b>",
-    "</a>",
-    "<ul>",
-    "<li>",
-    '<a href="#">yes</a>',
-    treeToHtml(tree.match),
-    "</li>",
-    "<li>",
-    '<a href="#">no</a>',
-    treeToHtml(tree.notMatch),
-    "</li>",
-    "</ul>",
-    "</li>",
-    "</ul>",
-  ].join("");
 }
