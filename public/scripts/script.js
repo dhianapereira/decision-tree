63b9fb643470d5da8085ea5f5117dae7554c2688
Samara Ferreira <ferreirasamara.sf@gmail.com>
2020-12-04 12:11:22 -0300

    fix

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -21,29 +21,29 @@ function loadData() {
     
     var decisionTreePrediction = decisionTree.predict(patient);
 
-    document.getElementById("testingItem").innerHTML =
-      "<td>" +
-      patient.age +
-      "</td>" +
-      "<td>" +
-      patient.menopause +
-      "</td>" +
-      "<td>" +
-      patient.tumorSize +
-      "</td>" +
-      "<td>" +
-      patient.degMalig +
-      "</td>" +
-      "<td>" +
-      patient.breast +
-      "</td>" +
-      "<td>" +
-      patient.breastQuad +
-      "</td>";
+    // document.getElementById("testingItem").innerHTML =
+    //   "<td>" +
+    //   patient.age +
+    //   "</td>" +
+    //   "<td>" +
+    //   patient.menopause +
+    //   "</td>" +
+    //   "<td>" +
+    //   patient.tumorSize +
+    //   "</td>" +
+    //   "<td>" +
+    //   patient.degMalig +
+    //   "</td>" +
+    //   "<td>" +
+    //   patient.breast +
+    //   "</td>" +
+    //   "<td>" +
+    //   patient.breastQuad +
+    //   "</td>";
 
-    document.getElementById(
-      "decisionTreePrediction"
-    ).innerHTML = decisionTreePrediction;
+    // document.getElementById(
+    //   "decisionTreePrediction"
+    // ).innerHTML = decisionTreePrediction;
 
     document.getElementById("displayTree").innerHTML = treeToHtml(
       decisionTree.root
